aa9dc93239fed31006d0aa6db386b3b6aaf4c6df
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-16 05:23:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.id = books.id JOIN characters ON characters.character_id = characters.id WHERE characters.species = 'human"
+  "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.id = books.id JOIN characters ON characters.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
